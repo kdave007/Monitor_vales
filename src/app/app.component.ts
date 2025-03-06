6a5cb3594021e64ac7c539e51984203b2461603d
kdave007 <kevin.maldonado.dev@gmail.com>
2025-03-06 12:11:01 -0600

    basic side bar nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule,RouterOutlet,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  @Input() isOpen: boolean = true;
+}
