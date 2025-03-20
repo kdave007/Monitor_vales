ccf1833f03ee6efff06f9a31bf4209701b48e9b5
kdave007 <kevin.maldonado.dev@gmail.com>
2025-03-20 16:05:14 -0600

    drop list added

--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -20,8 +20,24 @@ import { ValesStateTableComponent } from './vales-state-table/vales-state-table.
     ValesStateTableComponent
   ],
   templateUrl: './review.component.html',
-  styleUrl: './review.component.scss'
+  styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent {
+  states = [
+    { id: 1, name: 'Jalisco' },
+    { id: 2, name: 'Ciudad de México' },
+    { id: 3, name: 'Nuevo León' },
+    { id: 4, name: 'Guadalajara' },
+    { id: 5, name: 'Monterrey' }
+  ];
+  
+  selectedStateId = this.states[0].id;
 
+  onStateChange(event: any) {
+    this.selectedStateId = parseInt(event.target.value);
+  }
+
+  get currentState() {
+    return this.states.find(state => state.id === this.selectedStateId);
+  }
 }
